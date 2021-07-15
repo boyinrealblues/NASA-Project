0cea1b7c55b2631c8c5f29ea9cb4601a4970b67c
boyinbluehues <therealraghavkashyap26@gmail.com>
2021-07-15 14:49:37 +0530

    unused dependencies removed

--- app/src/main/java/com/example/nasa/UI/PhotoViewFragment.kt
+++ app/src/main/java/com/example/nasa/UI/PhotoViewFragment.kt
@@ -5,6 +5,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.appcompat.widget.Toolbar
 import androidx.databinding.DataBindingUtil
 import com.bumptech.glide.Glide
 import com.example.nasa.R
@@ -24,7 +25,7 @@ class PhotoViewFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-
+        activity?.findViewById<Toolbar>(R.id.toolbar)?.visibility = View.GONE
         arguments?.let {
             it.getString("123")?.let {
                 Glide.with(this).load(it).into(binding.photoViewer)
