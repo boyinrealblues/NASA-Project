47f2995b31f1f9f38c2cbbcf28e614ecfb63b6dc
boyinbluehues <therealraghavkashyap26@gmail.com>
2021-07-15 14:36:51 +0530

    Project NASA : APOD the most simple implentation made : bugs found

--- app/src/main/java/com/example/nasa/UI/ApodFragment.kt
+++ app/src/main/java/com/example/nasa/UI/ApodFragment.kt
@@ -2,6 +2,7 @@ package com.example.nasa.UI
 
 import android.content.Context
 import android.os.Bundle
+import android.text.TextUtils.replace
 import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
@@ -9,23 +10,27 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.ProgressBar
 import androidx.core.net.toUri
+import androidx.core.view.isVisible
 import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.commit
+import androidx.fragment.app.replace
 import androidx.lifecycle.ViewModelProvider
 import com.bumptech.glide.Glide
+import com.example.nasa.Data.APOD
 import com.example.nasa.R
 import com.example.nasa.databinding.FragmentApodBinding
 
 private const val TAG = "ApodFragment"
 class ApodFragment : Fragment() {
-
-   lateinit private var binding :FragmentApodBinding
+    lateinit private var mApod : APOD
+    lateinit private var binding: FragmentApodBinding
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-            binding = DataBindingUtil.inflate(inflater,R.layout.fragment_apod,container,false)
-            return binding.root
+        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_apod, container, false)
+        return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
@@ -33,31 +38,49 @@ class ApodFragment : Fragment() {
         val model = ViewModelProvider(this).get(APODViewModel::class.java)
         model.getAPOD()
         activity?.findViewById<ProgressBar>(R.id.progress_bar)?.visibility = View.VISIBLE
-
-        model.APODData.observe(viewLifecycleOwner,{
-            binding.textTitle.setText(it.title)
-            binding.date.setText(it.date)
-            binding.description.setText(it.explanation)
-            when(it.media_type) {
-               "video"-> {
-                   binding.imageView?.visibility= View.GONE
-                   binding.videoView.visibility = View.VISIBLE
-                   binding.videoView.setVideoURI(it.url.toUri())
-                   binding.videoView.setOnPreparedListener {
-                       binding.videoView.start()
-                   }
-               }
-
-                "image"-> {
-                    binding.imageView?.visibility = View.VISIBLE
-                    binding.videoView.visibility = View.GONE
-                    Glide.with(this).load(it.url).centerCrop().into(binding.imageView!!)
-                }
-
-            }
-
+        model.APODData.observe(viewLifecycleOwner, {
+            mApod = it
+            initApod(mApod)
             activity?.findViewById<ProgressBar>(R.id.progress_bar)?.visibility = View.GONE
         })
 
+        binding.header.setOnClickListener {
+            if(binding.imageView.isVisible&&::mApod.isInitialized)
+            {
+                val bundle : Bundle? = Bundle()
+                bundle?.putString("123",mApod.url)
+                parentFragmentManager.commit{
+                    replace<PhotoViewFragment>(R.id.container,args = bundle)
+                    addToBackStack(null)
+
+                }
+            }
+        }
     }
+
+    fun mediaType(dataSet: APOD) {
+        when (dataSet.media_type) {
+            "video" -> {
+                binding.imageView.visibility = View.GONE
+                binding.videoView.visibility = View.VISIBLE
+                binding.videoView.setVideoURI(dataSet.url.toUri())
+                binding.videoView.setOnPreparedListener {
+                    binding.videoView.start()
+                }
+            }
+
+            "image" -> {
+                binding.imageView.visibility = View.VISIBLE
+                binding.videoView.visibility = View.GONE
+                Glide.with(this).load(dataSet.url).centerCrop().into(binding.imageView)
+            }
+
+        }
+    }
+        fun initApod(dataSet : APOD){
+            binding.textTitle.setText(dataSet.title)
+            binding.date.setText(dataSet.date)
+            binding.description.setText(dataSet.explanation)
+            mediaType(dataSet)
+        }
 }
---
+++ app/src/main/java/com/example/nasa/UI/PhotoViewFragment.kt
@@ -0,0 +1,34 @@
+package com.example.nasa.UI
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import com.bumptech.glide.Glide
+import com.example.nasa.R
+import com.example.nasa.databinding.FragmentPhotoViewBinding
+
+class PhotoViewFragment : Fragment() {
+    lateinit private var binding: FragmentPhotoViewBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_photo_view, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        arguments?.let {
+            it.getString("123")?.let {
+                Glide.with(this).load(it).into(binding.photoViewer)
+            }
+        }
+    }
+}
