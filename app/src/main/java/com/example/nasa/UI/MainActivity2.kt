7553155fe322ca5cbd9d1d89e460272faa1a4bfc
boyinbluehues <therealraghavkashyap26@gmail.com>
2021-07-16 18:39:39 +0530

    big icon

--- app/src/main/java/com/example/nasa/UI/MainActivity2.kt
+++ app/src/main/java/com/example/nasa/UI/MainActivity2.kt
@@ -1,17 +1,17 @@
 package com.example.nasa.UI
 
 import android.graphics.drawable.AnimatedVectorDrawable
+import android.graphics.drawable.VectorDrawable
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.databinding.DataBindingUtil
 import androidx.vectordrawable.graphics.drawable.AnimatedVectorDrawableCompat
 import com.example.nasa.R
 import com.example.nasa.databinding.ActivityMain2Binding
-import com.example.nasa.databinding.ActivityMainBinding
 
 class MainActivity2 : AppCompatActivity() {
      lateinit private var binding : ActivityMain2Binding
-     override fun onCreate(savedInstanceState: Bundle?) {
+    override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(this,R.layout.activity_main2)
         binding.clicker.setOnClickListener{
@@ -25,5 +25,5 @@ class MainActivity2 : AppCompatActivity() {
                 }
             }
         }
-     }
+    }
 }
